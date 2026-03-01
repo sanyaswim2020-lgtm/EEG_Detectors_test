df2633237ee528ff6b03338f7e5d9f1fa55e3137
Ivan Glytov <ivanscibt@gmail.com>
2026-03-01 10:46:25 +0300

    ad preprocessing file

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,5 +1,5 @@
 """
-Input: raw_data
+Input: n3_intervals
 Output: preprocessed_data
 
 Process:
@@ -10,3 +10,35 @@ Process:
 - полосовая фильтрация
 - спектральное выравнивание?
 """
+
+
+import os 
+import mne 
+
+
+def preprocess_file(path_to_edf_file, LOW_FREQS, HIGH_FREQS):
+    """
+    path_to_edf_file - путь к целовому фрагменту сигнала
+    LOW_FREQS - нижний порог фильтрации
+    HIGH_FREQS - верхний порог фильтрации
+    """
+    interval = mne.io.read_raw_edf(path_to_edf_file, preload=True)
+
+    interval.notch_filter(freqs=50)
+
+    interval.filter(l_freq=LOW_FREQS, h_freq=HIGH_FREQS)
+
+    return interval 
+
+
+def main(path, sub_num, low_freqs, high_freqs):
+    interval = preprocess_file(path, low_freqs, high_freqs)
+    interval.save(f'data/filtered_data/sub_{sub_num}_preprocessed_raw.fif', overwrite=True, fmt='single')
+    
+
+if __name__ == '__main__':
+    path = '/Users/glytov_ivan/Documents/СНК/СНК ВМ/hfo_detectors_comparison_project/EEG_Detectors_test/data/n3_intervals/sub_01/sub-01_ses-01_task-hfo_run-01_eeg.edf'
+    sub_num = '01' # добавить автоматическое определение номера испытуемого
+    LOW_FREQS = 80
+    HIGH_FREQS = 500
+    main(path, sub_num, LOW_FREQS, HIGH_FREQS)
