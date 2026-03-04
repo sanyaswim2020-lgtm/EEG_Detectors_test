e9f76fb1909da9ab926f90a29f872ea99f71ed64
sanyaswim2020-lgtm <alekschem2100@gmail.com>
2026-03-05 00:03:51 +0300

    add Hilbert's envelope in primary filtration

--- detector/primary_filtration.py
+++ detector/primary_filtration.py
@@ -10,3 +10,35 @@ Process:
 - сохранять начало и конец событий 
 - вырезать события из raw_data и preprocessed_data и сохранить их
 """
+import os 
+import matplotlib
+import mne 
+import numpy as np
+mne.viz.set_browser_backend('qt')
+
+# путь к файлу из filtered data
+path_to_fif_file = "/home/dmin/Code/EEG_Detectors_test/data/filtered_data/sub_01_preprocessed_raw.fif"
+interval = mne.io.read_raw_fif(path_to_fif_file, preload=True)
+
+# Исправление багнутого файла с амплитудой в вольтах
+data = interval.get_data()
+print(f"Максимум: {data.max()}, Минимум: {data.min()}, Среднее: {data.mean()}")
+print(interval.get_channel_types())
+interval.apply_function(lambda x: x * 1e-6)
+interval.plot()
+
+# Расчёт energy-based метрик
+LOW_FREQS_ripples = 80
+HIGH_FREQS_ripples = 250
+LOW_FREQS_fast_ripples = 250
+HIGH_FREQS_fast_ripples = 500
+
+interval_ripples = interval.copy().filter(l_freq=LOW_FREQS_ripples, h_freq=HIGH_FREQS_ripples)
+interval_fast_ripples = interval.copy().filter(l_freq=LOW_FREQS_fast_ripples, h_freq=HIGH_FREQS_fast_ripples)
+
+hilbert_ripples = interval_ripples.apply_hilbert(envelope=True)
+hilbert_fast_ripples = interval_fast_ripples.apply_hilbert(envelope=True)
+
+hilbert_ripples.plot()
+hilbert_fast_ripples.plot()
+
--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -34,10 +34,11 @@ def preprocess_file(path_to_edf_file, LOW_FREQS, HIGH_FREQS):
 def main(path, sub_num, low_freqs, high_freqs):
     interval = preprocess_file(path, low_freqs, high_freqs)
     interval.save(f'data/filtered_data/sub_{sub_num}_preprocessed_raw.fif', overwrite=True, fmt='single')
-    
+    interval.plot()
+
 
 if __name__ == '__main__':
-    path = '/Users/glytov_ivan/Documents/СНК/СНК ВМ/hfo_detectors_comparison_project/EEG_Detectors_test/data/n3_intervals/sub_01/sub-01_ses-01_task-hfo_run-01_eeg.edf'
+    path = "/home/dmin/Code/EEG_Detectors_test/data/n3_intervals/sub_01/sub-01_ses-01_task-hfo_run-01_eeg.edf"
     sub_num = '01' # добавить автоматическое определение номера испытуемого
     LOW_FREQS = 80
     HIGH_FREQS = 500
