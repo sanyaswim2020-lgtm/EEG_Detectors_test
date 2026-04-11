9fbf527aeb671334c4bde14b71b63ceae585f26f
sanyaswim2020-lgtm <alekschem2100@gmail.com>
2026-04-11 20:48:56 +0300

    add files

---
+++ main.py
@@ -0,0 +1,19 @@
+# импорт функций
+from preprocessing.preprocessing import 
+from detector.primary_filtration import 
+from detector.validation import 
+# Предположим, у тебя там есть такой скрипт:
+# from visualisations.plotter import plot_metrics 
+
+def run_eeg_pipeline(file_path):
+    
+    # Этап 1: Препроцессинг
+    
+    # Этап 2: Детектор
+    
+    # Этап 3: Визуализация 
+    
+    metrics_results = 
+    return metrics_results
+
+if __name__ == "__main__":
